8bd0f078e08595c3dff0a482587b8b23d2635f65
asdasd <jufranhundo@gmail.com>
2023-02-12 20:28:37 +0800

    add table

--- week9/database.php
+++ week9/database.php
@@ -11,7 +11,14 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-
+$sql = "CREATE TABLE cpbustamante_guestlist (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(30) NOT NULL,
+  lastname VARCHAR(30) NOT NULL,
+  email VARCHAR(50),
+  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  )";
+  
 
 $conn->close();
 ?>
